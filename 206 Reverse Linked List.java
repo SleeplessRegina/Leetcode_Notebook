67b507074c0326b5a2bf86caff837e2f50f6a4fa
Xuanying Wang <wang.xuanyi@northeastern.edu>
2022-12-28 17:35:21 -0800

    create 206 Reverse Linked List

---
+++ 206 Reverse Linked List.java
@@ -0,0 +1,30 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// 1 -> 2 -> 3 -> 4 -> 5 
+// 5 -> 4 -> 3 -> 2 -> 1
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+ //recursive
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while(head != null){
+            ListNode nextNode = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextNode;
+        }
+        return prev;
+    }
+}
